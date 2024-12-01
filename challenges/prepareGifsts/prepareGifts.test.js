37d8807af1b5635e88c3bad8d5488b5feb58692f
delellisgabriel <delellisgabriel@gmail.com>
2024-12-01 22:20:20 +0100

    test: adds prepareGifts function tests

---
+++ challenges/prepareGifsts/prepareGifts.test.js
@@ -0,0 +1,22 @@
+import { describe, test, expect } from "vitest";
+import prepareGifts from "./prepareGifts";
+
+const testCases = [
+  { input: [3, 1, 2, 3, 4, 2, 5], expected: [1, 2, 3, 4, 5] },
+  { input: [6, 5, 5, 5, 5], expected: [5, 6] },
+  { input: [], expected: [] },
+];
+
+describe("Challenge #1 🎁 First gift repeated!", () => {
+  //  Santa Claus 🎅 has received a list of magical numbers representing gifts 🎁,
+  //  but some of them are duplicated and must be removed to avoid confusion.
+  //  Additionally, the gifts must be sorted in ascending order before being delivered to the elves.
+  //  Your task is to write a function that receives a list of integers (which may include duplicates) and returns a new list without duplicates, sorted in ascending order.
+  test.each(testCases)(
+    "gifts are sorted & not duplicated",
+    ({ input, expected }) => {
+      const sortedGifts = prepareGifts(input);
+      expect(sortedGifts).toEqual(expected);
+    }
+  );
+});
