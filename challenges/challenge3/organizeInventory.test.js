fd784455121d0800fa936209f8ade1c43feaddd5
delellisgabriel <delellisgabriel@gmail.com>
2024-12-07 13:35:48 +0100

    chore: adds organizeInventory instructions

--- challenges/challenge3/organizeInventory.test.js
+++ challenges/challenge3/organizeInventory.test.js
@@ -1,6 +1,19 @@
 import { describe, test, expect, it } from "vitest";
 import { organizeInventory } from "./organizeInventory";
 
+// Santa Claus 🎅 is checking his workshop inventory to prepare gift delivery. The elves have recorded the toys in an array of objects, but the information is a bit disorganized. You need to help Santa organize the inventory.
+
+// You will receive an array of objects, where each object represents a toy and has the properties:
+
+// name: the name of the toy (string).
+// quantity: the available quantity of that toy (integer).
+// category: the category to which the toy belongs (string).
+// Write a function that processes this array and returns an object that organizes the toys as follows:
+
+// The keys of the object will be the categories of toys.
+// The values will be objects that have the toy names as keys and the total quantities of each toy in that category as values.
+// If there are toys with the same name in the same category, you must sum their quantities.
+// If the array is empty, the function should return an empty object {}.
 describe("Challenge #3: Organizing the inventory", () => {
   it("If the inventory is empty, the function should return an empty object {}", () => {
     const input = [];
